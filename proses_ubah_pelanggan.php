cc7c5cb78c798e4a2b287cb18f0c7b7282bbc7f8
adindhaprahesthy29rpl <72968347+adindhaprahesthy29rpl@users.noreply.github.com>
2021-09-20 22:43:05 +0700

    Add files via upload
    Berisi mengenai update data

---
+++ proses_ubah_pelanggan.php
@@ -0,0 +1,45 @@
+<?php
+
+if($_POST){
+
+    $id_pelanggan=$_POST['id_pelanggan'];
+
+    $nama=$_POST['nama'];
+
+    $alamat=$_POST['alamat'];
+
+    $telp=$_POST['telp'];
+
+
+    if(empty($nama)){
+
+        echo "<script>alert('nama tidak boleh kosong');location.href='tambah_pelanggan.php';</script>";
+
+
+    } elseif(empty($alamat)){
+
+        echo "<script>alert('alamat tidak boleh kosong');location.href='tambah_pelanggan.php';</script>";
+
+    } elseif(empty($telp)){
+
+        echo "<script>alert('telp tidak boleh kosong');location.href='tambah_pelanggan.php';</script>";
+
+    } else {
+
+        include "koneksii.php";
+            
+
+            $update=mysqli_query($conn,"update pelanggan set nama='".$nama."',alamat='".$alamat."', telp='".$telp."'where id_pelanggan = '".$id_pelanggan."' ") or die(mysqli_error($conn));
+
+            if($update){
+
+                echo "<script>alert('Sukses update pelanggan');location.href='tampil_pelanggan.php';</script>";
+
+            } else {
+
+                echo "<script>alert('Gagal update pelanggan');location.href='ubah_pelanggan.php?id_pelanggan=".$id_pelanggan."';</script>";    
+            }
+        }
+    }
+?>
+
